d67a3f6a5fb38a32c2f3aa76806f33ca8cdb8c6d
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-02-20 20:52:44 +0100

    Theme provider

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useReducer, lazy, Suspense, useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Loader from "./components/Loader";
+import { ThemeProvider, useTheme, createTheme } from "@mui/material";
 
 // Context
 import { initialUIState, UIReducer } from './context/UIContext';
@@ -13,6 +13,7 @@ export const PostContext = createContext()
 export const ChatContext = createContext()
 
 // Components
+import Loader from "./components/Loader";
 const Auth = lazy(() => import('./screens/Auth'))
 
 function App() {
@@ -21,25 +22,42 @@ function App() {
   const [userState, userDispatch] = useReducer(UserReducer, initialUserState)
   const [postState, postDispatch] = useReducer(PostReducer, initialPostState)
   const [chatState, chatDispatch] = useReducer(ChatReducer, initialChatState)
+  const Theme = React.useMemo(() => createTheme({
+      active: {
+        success: 'rgb(63,162,76)',
+      },
+      palette: {
+        type: uiState.darkMode ? 'dark' : 'light',
+        primary: {
+          main: 'rgb(1,133,243)',
+        },
+        secondary: {
+          main: 'rgb(63,162,76)',
+        },
+      },
+    }), [uiState.darkMode])
+  const theme = useTheme()
   return (
     <UIContext.Provider value={{ uiState, uiDispatch }}>
       <UserContext.Provider value={{ userState, userDispatch }}>
         <PostContext.Provider value={{ postState, postDispatch }}>
           <ChatContext.Provider value={{ chatState, chatDispatch }}>
-            <BrowserRouter>
-              <div style={{
-                  backgroundColor: !uiState.darkMode ? 'rgb(240,242,245)' : 'rgb(24,25,26)',
-                }}
-              >
-                <Suspense fallback={<Loader />}>
-                  {loading ? ( <Loader /> ) : (
-                    <Routes>
-                      <Route path='/' element={!userState.isLoggedIn ? ( <Auth /> ) : ( <Navigate to='/home' /> )} />
-                    </Routes>
-                  )}
-                </Suspense>
-              </div>
-            </BrowserRouter>
+            <ThemeProvider theme={Theme}>
+              <BrowserRouter>
+                <div style={{
+                    backgroundColor: !uiState.darkMode ? 'rgb(240,242,245)' : 'rgb(24,25,26)',
+                  }}
+                >
+                  <Suspense fallback={<Loader />}>
+                    {loading ? ( <Loader /> ) : (
+                      <Routes>
+                        <Route path='/' element={!userState.isLoggedIn ? ( <Auth /> ) : ( <Navigate to='/home' /> )} />
+                      </Routes>
+                    )}
+                  </Suspense>
+                </div>
+              </BrowserRouter>
+            </ThemeProvider>
           </ChatContext.Provider>
         </PostContext.Provider>
       </UserContext.Provider>

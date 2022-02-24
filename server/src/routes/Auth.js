0b319103ee4d2b6fd7fb10103d32ca47470c19d0
Gerset Linarez <58746126+RedCode26@users.noreply.github.com>
2022-02-24 01:11:16 +0100

    Logout

--- server/src/routes/Auth.js
+++ server/src/routes/Auth.js
@@ -1,8 +1,11 @@
 const Router = require('express').Router();
 const SignupUser = require('../controllers/Auth/Signup')
 const LoginUser = require('../controllers/Auth/Login')
+const Logout = require('../controllers/Auth/Logout')
+const authRequired = require("../middleware/AuthRequired")
 
 Router.post('/signup', SignupUser)
 Router.post('/login', LoginUser)
+Router.get("/logout",authRequired,Logout)
 
 module.exports = Router;
